84b9b9f9ff3060b933baf98b966795ef88e9af65
Love Borgström <loveborgstrom98@gmail.com>
2026-02-06 18:57:18 +0100

    Valid syntax

--- test/expected/09_case_statement.sh
+++ test/expected/09_case_statement.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-case $1 in start);echo "starting";echo "started";;stop);echo "stopping";echo "stopped";;esac;
+case $1 in start) echo "starting";echo "started";;stop) echo "stopping";echo "stopped";;esac;
